6af8cf74c6f93509cc06a9b71ff827e13febe9e9
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2009-05-02 23:58:59 +0200

    removed unnecessary torrent download abstraction

--- lib/f/finder/piratebay.rb
+++ lib/f/finder/piratebay.rb
@@ -1,15 +1,5 @@
 require 'tempfile'
 
-class F::Prompt
-  def with_torrent_file_from(url)
-    Tempfile.open 'f.piratebay' do |f|
-      f.write(http.get(http.get(url).at('.download a')['href']).content)
-      f.rewind
-      yield(f)
-    end
-  end
-end
-
 F::Finder.define :piratebay do
   description 'Pirate Bay torrent search'
   base_uri 'http://thepiratebay.org/'
@@ -18,7 +8,10 @@ F::Finder.define :piratebay do
   command :t, 'download torrent file and open it with BITTORRENT_CLIENT', 0 => :result_for_index do |result|
     torrent = ENV['BITTORRENT_CLIENT']
     raise 'environment variable BITTORRENT_CLIENT is not set or empty' if torrent.blank?
-    with_torrent_file_from result.url do |f|
+
+    Tempfile.open 'f.piratebay' do |f|
+      f.write(http.get(http.get(result.url).at('.download a')['href']).content)
+      f.rewind
       system("#{torrent} #{f.path}")
     end
   end
