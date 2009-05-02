e7bf55bc453480f7ec966f216724c98f66db1d4c
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2009-05-03 01:36:13 +0200

    IMDB movie title search

---
+++ lib/f/finder/imdb.rb
@@ -0,0 +1,14 @@
+F::Finder.define :imdb do
+  description 'IMDB movie title search'
+  base_uri 'http://www.imdb.com/'
+  find { |http, o| http.get '/find', :q => o[:args].join('+'), :s => 'tt' }
+
+  parse do |page, result|
+    result.header = 'Results for ' + page.at('input[name="q"]')['value']
+    page.css('td > a[href^="/title/"]').each do |a|
+      next if a.text.blank?
+      result << [a.parent.text.squeeze, a['href']]
+    end
+  end
+
+end
