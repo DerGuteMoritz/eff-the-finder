809e196d46149c0cacaccf06b81c5d188d134719
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2009-05-02 18:09:46 +0200

    graceful handling of invalid indices

--- lib/f/prompt.rb
+++ lib/f/prompt.rb
@@ -5,6 +5,14 @@ HighLine.track_eof = false
 
 class F::Prompt
 
+  class InvalidIndex < StandardError
+
+    def initialize(index)
+      super "`#{index}' is not a valid index."
+    end
+
+  end
+
   @@commands = {
     :f => [ 'follow link',
             lambda { |r| Launchy.open(r.url.to_s) } ],
@@ -68,16 +76,18 @@ class F::Prompt
     command = method(command) unless command.respond_to? :call
     return not_enough_arguments(command, args) if args.size < command.arity
     command.call(*args) || true
+  rescue InvalidIndex => e
+    puts e
   end
 
   def parse_command_string(s)
     if s =~ /\A(\d+)\z/
       c = @@commands[:f]
-      args = [@results[$1.to_i-1]]
+      args = [result_for_index($1)]
     else
       c = @@commands[s[/\A([a-z]+)/, 1].try(:to_sym)]
       args = Array(s[/\A[a-z]+(.*)/, 1].to_s.strip.split(/\s/))
-      args[0] = @results[args[0].to_i-1] if args[0].to_s =~ /\A\d+\z/
+      args[0] = result_for_index(args[0]) if args[0].to_s =~ /\A\d+\z/
     end
 
     [c && c[1], args]
@@ -94,6 +104,10 @@ class F::Prompt
 
   def not_enough_arguments(c, a)
     puts "not enough arguments (#{a.size} for #{c.arity})"
+  end
+
+  def result_for_index(i)
+    @results[i.to_i-1] || raise(InvalidIndex.new(i))
   end
 
 end
